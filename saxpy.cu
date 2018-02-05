2ff89c7b6655e35e475ed4d3a6a798b1ec16e0aa
mabergen <maribergendahl3@gmail.com>
2018-02-05 11:29:45 +0100

    Added some comments

--- saxpy.cu
+++ saxpy.cu
@@ -5,7 +5,8 @@
 __global__
 void saxpy(int n, float a, float *x, float *y){
 	int i = blockIdx.x*blockDim.x + threadIdx.x;
-	if (i < n) y[i] = a*x[i] + y[i];
+	if (i < n)  // ensure there are no out-of-bounds memory accesses
+		y[i] = a*x[i] + y[i];
 }
 
 
